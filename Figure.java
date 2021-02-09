9e3bf53fdec285d969df840920055f922f54936e
Daniel Maciejewski <46847771+SymmetryCyber@users.noreply.github.com>
2021-02-09 16:27:14 -0600

    Update Figure.java

--- Figure.java
+++ Figure.java
@@ -6,34 +6,37 @@ public class Figure
    
     Scanner stdIn = new Scanner(System.in);
     int n;
+    
     do
     {
-    System.out.print("Enter n [1, 9] : ");
-    n = stdIn.nextInt();
+		System.out.print("Enter n [1, 9] : ");
+		n = stdIn.nextInt();
     } while ( n < 1 && n > 9 );
+    
     System.out.println();
     for (int line = 1; line <= n; ++line)
     {
-    for (int s = 1; s <= line; ++s)
-    System.out.print(' ');
-    for (int ld = 1; ld <= line; ++ld)
-    System.out.print(ld);
-    for (int rd = 1; rd <= line; ++rd)
-    System.out.print(rd);
-    System.out.println();
-    }
+		for (int s = 1; s <= line; ++s)
+		System.out.print(' ');
+		for (int ld = 1; ld <= line; ++ld)
+		System.out.print(ld);
+		for (int rd = 1; rd <= line; ++rd)
+		System.out.print(rd);
+		System.out.println();
+    }//end for
+	  
     for (int line = 1; line <= n; ++line)
     {
-    for (int s = 1; s <= line; ++s)
-    System.out.print(' ');
-    for (int ld = 1; ld <= line; ++ld)
-    System.out.print(ld);
-    for (int rd = 1; rd <= line; ++rd)
-    System.out.print(rd);
-    System.out.println();
-    }
+		for (int s = 1; s <= line; ++s)
+		System.out.print(' ');
+		for (int ld = 1; ld <= line; ++ld)
+		System.out.print(ld);
+		for (int rd = 1; rd <= line; ++rd)
+		System.out.print(rd);
+		System.out.println();
+    }//end for
     stdIn.close();
-    }
+    }//end main
     
   
-}
+}//end class
